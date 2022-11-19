c1a220cb8c716eebb55dd25060b911c7c24f1d20
Ronnakon <ronnakon_nisaensat@cmu.ac.th>
2022-11-19 12:41:24 +0700

    Home-login

--- src/Menu/Home.js
+++ src/Menu/Home.js
@@ -5,6 +5,8 @@ import axios from 'axios';
 
 export default function Home() {
     const [products, setProducts] = useState([]);
+    const [token, setToken] = useState('');
+
     useEffect(() => {
         async function getProducts() {
             const products = await axios.get(
@@ -13,8 +15,15 @@ export default function Home() {
             setProducts(products.data);
         }
         getProducts();
+        setToken(JSON.parse(localStorage.getItem('token')));
     }, []);
-    console.log(products);
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        token ? window.location.href = '/select' : window.location.href = '/login'
+
+    }
+
     return (
         <>
             <div className='bg-color'>
@@ -48,31 +57,35 @@ export default function Home() {
                 <div>
                     <p class="text-center fw-bold card-body3 p-5 fs-2 ps-5">PERFUME</p>
                 </div>
+
                 <div className='col-12 d-flex justify-content-center'>
-                    <div className='p-4 p-sm-5'>
-                        <div className="card mb-3" style={{ maxWidth: 540 }}>
-                            <div className="row g-0">
-                                <div className="col-md-4 btn-pp align-items-center d-flex">
-                                    <div class="d-flex ">
-                                        <img src='./img/113.png' className="img-fluid rounded-start" alt="..." />
+                    {products?.map((product) =>
+                        <div className='p-4 p-sm-5'>
+                            <div className="card mb-3" style={{ maxWidth: 540 }}>
+                                <div className="row g-0">
+                                    <div className="col-md-4 btn-pp align-items-center d-flex">
+                                        <div class="d-flex ">
+                                            <img src='./img/113.png' className="img-fluid rounded-start" alt="..." />
+                                        </div>
                                     </div>
-                                </div>
-                                <div className="col-md-8 ">
-                                    <div className="card-body bg-cream">
-                                        <h5 className="card-title">Card title</h5>
-                                        <p className="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
-                                        <hr></hr>
-                                        <p className="card-text">Brand Product</p>
-                                        <hr></hr>
-                                        <div className='d-flex justify-content-md-center'>
-                                            <button class="btn-or btn badge text-uppercase fs-6" type="button">get free</button>
+                                    <div className="col-md-8 ">
+                                        <div className="card-body bg-cream">
+                                            <h5 className="card-title">{product.productName}</h5>
+                                            <p className="card-text">{product.productDescription}</p>
+                                            <hr></hr>
+                                            <p className="card-text">Brand Product</p>
+                                            <hr></hr>
+                                            <div className='d-flex justify-content-md-center'>
+                                                <form onSubmit={handleSubmit}><button class="btn-or btn badge text-uppercase fs-6" type="submit">get free</button></form>
+                                            </div>
                                         </div>
                                     </div>
                                 </div>
                             </div>
                         </div>
-                    </div>
-                    <div className='p-4 p-sm-5'>
+                    )}
+
+                    {/* <div className='p-4 p-sm-5'>
                         <div className="card mb-3" style={{ maxWidth: 540 }}>
                             <div className="row g-0">
                                 <div className="col-md-4 btn-pp align-items-center d-flex">
@@ -93,7 +106,7 @@ export default function Home() {
                                 </div>
                             </div>
                         </div>
-                    </div>
+                    </div> */}
                 </div>
                 <Footer />
             </div>
