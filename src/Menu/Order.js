bfce054bc47e12294c97073b279d6544be418506
Ronnakon <ronnakon_nisaensat@cmu.ac.th>
2022-11-21 15:44:47 +0700

    before merg

--- src/Menu/Order.js
+++ src/Menu/Order.js
@@ -8,6 +8,7 @@ export default function Order() {
     const token = JSON.parse(localStorage.getItem('token'));
     const { productName } = useParams();
     const [receipt, setReceipt] = useState('');
+    const [receiptStat, setReceiptStat] = useState('')
     useEffect(() => {
         async function getOrder() {
             const orders = await axios.get(
@@ -17,13 +18,14 @@ export default function Order() {
                 }
             }
             ).catch(function (err) {
-                console.log(err)
+
+                setReceiptStat(err.response.data)
             });
             setReceipt(orders?.data);
         }
         getOrder();
     }, [token]);
-    console.log(receipt)
+    console.log(receiptStat)
 
     if (!token) {
         window.location.href = "/login";
@@ -36,10 +38,8 @@ export default function Order() {
             return (
                 <Select productName={productName} />
             );
-        } else if (!productName && !receipt) {
-            return (
-                <Home />
-            );
+        } else if (receiptStat === "No Order") {
+            window.location.href = '/';
         }
 
     }
