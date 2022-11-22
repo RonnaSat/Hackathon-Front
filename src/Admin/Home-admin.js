6234f5231ef59d2759af07fd6788040fc032310b
Ronnakon <ronnakon_nisaensat@cmu.ac.th>
2022-11-22 14:45:10 +0700

    modal

--- src/Admin/Home-admin.js
+++ src/Admin/Home-admin.js
@@ -2,13 +2,16 @@ import React, { useEffect, useState } from 'react';
 import Sidebar from '../Component/Sidebar';
 import axios from 'axios';
 import { Link } from 'react-router-dom';
-import ProductEditModal from '../Component/modalDivContent';
+import AdminProductBox from '../Component/adminProductBox';
+
+
+
+
 export default function Home_product() {
     const token = JSON.parse(localStorage.getItem('tokenAdmin'));
     const [products, setProducts] = useState([]);
 
     if (!token) window.location.href = 'signin-admin';
-
     useEffect(() => {
         async function getProducts() {
             const products = await axios.get(
@@ -20,26 +23,10 @@ export default function Home_product() {
         getProducts();
     }, [token]);
 
-    function handleClick(product) {
-        console.log(product._id);
-        axios.delete(`http://localhost:8000/product/${product._id}`, {
-            headers: {
-                authorization: token
-            }
-        })
-            .then((response) => {
-                console.log(response);
-                alert("Delete success");
-                window.location.reload();
-            }).catch((error) => {
-                console.log(error);
-                alert("Delete error");
-            })
-
-    }
 
 
-    console.log(products);
+
+
     return (
         <>
             <Sidebar />
@@ -65,61 +52,10 @@ export default function Home_product() {
                             </div>
                         </div>
                     </div>
-                    {products?.map((product) =>
-                        <div className="container d-flex justify-content-center col-12">
-                            <div class="row d-flex justify-content-center pb-3 col-12">
-                                <div class="col-8">
-                                    <div class="p-4 border bg-pp rounded-3 mb-5">
-                                        <div class="container">
-                                            <div class="row">
-                                                <div class="col rounded-3 col-2">
-                                                    <div class="d-flex align-items-center">
-                                                        <img src={product.productImageBase64} className="img-fluid d-flex h-100" alt="..." />
-                                                    </div>
-                                                </div>
-                                                <div class="col m-2">
-                                                    <h4 className="card-title pt-3 fw-bold pb-2">{product.productName}</h4>
-                                                    <p className="card-text ">{product.productDescription}</p>
-                                                    <hr></hr>
-                                                    <h5 className="card-title fw-bold pb-2">Branch</h5>
-                                                    <p className="card-text">{product.productLocation}</p>
-                                                </div>
-                                                <div class="row col-3 ">
-                                                    <div class="col col-6 d-flex justify-content-end align-items-center" >
-                                                        {/* <Link to='/add_product' class="bi bi-pencil-square fs-3"></Link> */}
-                                                        <div>
-                                                            {/* Button trigger modal */}
-                                                            <button type="button" className="btn btn-primary" data-bs-toggle="modal" data-bs-target="#exampleModal">
-                                                                Launch demo modal
-                                                            </button>
-                                                            {/* Modal */}
-                                                            <div className="modal fade" id="exampleModal" tabIndex={-1} aria-labelledby="exampleModalLabel" aria-hidden="true">
-                                                                <div className="modal-dialog modal-xl">
-                                                                    <div className="modal-content">
-                                                                        <ProductEditModal product={product} token={token} />
-                                                                        <div className="modal-footer">
-                                                                            <button type="button" className="btn btn-secondary" data-bs-dismiss="modal">Close</button>
-                                                                            <button type="button" className="btn btn-primary">Save changes</button>
-                                                                        </div>
-                                                                    </div>
-                                                                </div>
-                                                            </div>
-                                                        </div>
 
-                                                    </div>
-                                                    <div class="col col-6 d-flex justify-content-center align-items-center">
-                                                        <form>
-                                                            <input type="hidden" />
-                                                            <div type='submit' class="bi bi-trash-fill fs-3" onClick={() => handleClick(product)}></div>
-                                                        </form>
-                                                    </div>
-                                                </div>
-                                            </div>
-                                        </div>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>)}
+                    {products?.map((product) =>
+                        <AdminProductBox product={product} />
+                    )}
                 </div>
             </div>
         </>
